2e39114a577330ecebbce226580289c645f3580a
GeorgianF <georgianfurtuna@yahoo.com>
2022-05-07 21:03:56 +0000

    add summernote to the admin panel

--- codestar/settings.py
+++ codestar/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'blog',
+    'django_summernote',
 ]
 
 MIDDLEWARE = [
---
+++ env_sample.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ['DATABASE_URL'] = 'postgres://ekhydqocjtcucy:b6261e914200244a6350cc81c7f38a530cc6aba434610c3449bbc23cc08c3cea@ec2-176-34-211-0.eu-west-1.compute.amazonaws.com:5432/d38146i48ripmi'
+os.environ['SECRET_KEY'] = 'randomSecretKey1988!!'
+os.environ['CLOUDINARY_URL'] = 'cloudinary://646375447921126:oD5ObcVSyFKRAeX_fk1tgYImU04@dlyr5slol'
