9932a818747026334bbac2932506f2cf34a4de70
GeorgianF <georgianfurtuna@yahoo.com>
2022-05-10 21:16:54 +0000

    add html templates and set up urls

--- blog/admin.py
+++ blog/admin.py
@@ -5,18 +5,20 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
-    search_fields = ['title', 'content']
+
     list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
     list_filter = ('status', 'created_on')
     prepopulated_fields = {'slug': ('title',)}
-    summernote_fields = 'content'
+    summernote_fields = ('content',)
+
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    search_fields = ['name', 'email', 'body']
     list_display = ('name', 'body', 'post', 'created_on', 'approved')
-    list_filter = ('created_on', 'approved')
-    actions = ["approved_comments"]
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
 
-    def approved_comments(self, request, queryset):
+    def approve_comments(self, request, queryset):
         queryset.update(approved=True)
