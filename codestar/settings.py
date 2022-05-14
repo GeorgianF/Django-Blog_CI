5e6c7006c08fa2fb6e4f227bdffadc7338f99a29
GeorgianF <georgianfurtuna@yahoo.com>
2022-05-14 15:18:07 +0000

    add ability to like posts and add comments

--- codestar/settings.py
+++ codestar/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
-
+from django.contrib.messages import constants as messages
 import dj_database_url
 if os.path.isfile("env.py"):
     import env
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'cloudinary',
     'blog',
     'django_summernote',
+    'crispy_forms',
 ]
 
 SITE_ID = 1
@@ -55,6 +56,16 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
 ACCOUNT_EMAIL_VERIFICATION = 'none'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+MESSAGE_TAGS = {
+        messages.DEBUG: 'alert-info',
+        messages.INFO: 'alert-info',
+        messages.SUCCESS: 'alert-success',
+        messages.WARNING: 'alert-warning',
+        messages.ERROR: 'alert-danger',
+    }
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
